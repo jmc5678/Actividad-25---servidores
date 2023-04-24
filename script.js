fe27222931cccb19ac5ec2efc44d282fe597acd8
jare <125687149+jmc5678@users.noreply.github.com>
2023-04-24 09:00:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+ var x = {a:120};
+            var i;
+                for (i in x){ 
+                    document.write("vino tinto" + " : " + x [ i ] + "<br>"); 
+                   
+                
+                }
+var x = {a:400};
+            var i;
+                for (i in x){ 
+                    document.write("Tequila" + " : " + x [ i ] + "<br>"); 
+                   
+                
+                } 
+var x = {a:600};
+            var i;
+                for (i in x){ 
+                 document.write("Whisky Buchanan's" + " : " + x [ i ] + "<br>");
+                   
+                
+               }
